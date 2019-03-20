bc48ebdf31f080d379c1312c624dfd96f8eae879
木叶 <869752664@qq.com>
2019-03-20 22:04:40 +0800

    Site updated: 2019-03-20 22:04:39

--- live2d/autoload.js
+++ live2d/autoload.js
@@ -1,3 +1,3 @@
-// build time:Wed Mar 20 2019 21:04:40 GMT+0800 (GMT+08:00)
+// build time:Wed Mar 20 2019 22:04:34 GMT+0800 (GMT+08:00)
 const live2d_path="/live2d/";$("<link>").attr({href:live2d_path+"waifu.css",rel:"stylesheet",type:"text/css"}).appendTo("head");$.ajax({url:live2d_path+"live2d.min.js",dataType:"script",cache:true,async:false});$.ajax({url:live2d_path+"waifu-tips.js",dataType:"script",cache:true,async:false});$(window).on("load",function(){initWidget(live2d_path+"waifu-tips.json","https://live2d.fghrsh.net/api")});
 //rebuild by neat 
