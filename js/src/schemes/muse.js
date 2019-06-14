2a6c725f9d3826126f3c83fb827f4ec37ce436f3
木叶 <869752664@qq.com>
2019-06-14 09:24:44 +0800

    Site updated: 2019-06-14 09:24:42

--- js/src/schemes/muse.js
+++ js/src/schemes/muse.js
@@ -1,3 +1,3 @@
-// build time:Thu Jun 06 2019 13:59:25 GMT+0800 (GMT+08:00)
+// build time:Fri Jun 14 2019 09:24:36 GMT+0800 (GMT+08:00)
 $(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
 //rebuild by neat 
