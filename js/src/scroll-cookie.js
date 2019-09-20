1c83bab3cd51e18e39c4a146e8c0f963e78cd279
木叶 <869752664@qq.com>
2019-09-20 16:53:52 +0800

    Site updated: 2019-09-20 16:53:51

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Thu Sep 05 2019 11:03:30 GMT+0800 (GMT+08:00)
+// build time:Fri Sep 20 2019 16:53:35 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
