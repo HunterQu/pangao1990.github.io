b129781297674a7080e094176f4722c2d1854622
潘高 <pangao1990@qq.com>
2019-09-22 15:35:17 +0800

    Site updated: 2019-09-22 15:35:15

--- live2d/autoload.js
+++ live2d/autoload.js
@@ -1,3 +1,3 @@
-// build time:Sun Sep 22 2019 15:30:06 GMT+0800 (GMT+08:00)
+// build time:Sun Sep 22 2019 15:35:05 GMT+0800 (GMT+08:00)
 const live2d_path="/live2d/";$("<link>").attr({href:live2d_path+"waifu.css",rel:"stylesheet",type:"text/css"}).appendTo("head");$.ajax({url:live2d_path+"live2d.min.js",dataType:"script",cache:true,async:false});$.ajax({url:live2d_path+"waifu-tips.js",dataType:"script",cache:true,async:false});$(window).on("load",function(){initWidget(live2d_path+"waifu-tips.json","https://live2d.fghrsh.net/api")});
 //rebuild by neat 
