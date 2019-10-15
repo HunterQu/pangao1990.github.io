188637c70f898ae2d0f21896490f7fabca7e2b91
潘高 <pangao1990@qq.com>
2019-10-15 22:05:57 +0800

    Site updated: 2019-10-15 22:05:54

--- js/src/schemes/muse.js
+++ js/src/schemes/muse.js
@@ -1,3 +1,3 @@
-// build time:Mon Oct 14 2019 23:25:47 GMT+0800 (GMT+08:00)
+// build time:Tue Oct 15 2019 22:05:45 GMT+0800 (GMT+08:00)
 $(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
 //rebuild by neat 
